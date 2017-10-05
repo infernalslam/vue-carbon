0da0ad3be559aa404eaf124898f1a26330771288
infernalslam <infernal-slam@hotmail.com>
2017-10-05 12:40:08 +0700

    css

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="index-bg-color"> Hello World </div>
+  <div>
+    <div class="index-bg-text">
+      <span class="index-title-text" >Vue Carbon</span> <br>
+      <span class="index-subtitle-text">
+        Create and share beautiful images of your source code.
+        Start typing, or drop a file into the text area to get started.
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,7 +25,18 @@ export default {
 </script>
 
 <style scoped>
-.index-bg-color {
-  background-color: #121212;
+.index-bg-text {
+  color: #f8e81d;
+  text-align: center;
+  padding-top: 5%;
+  padding-left: 20%;
+  padding-right: 20%;
+}
+.index-title-text {
+  font-size: 50px;
+}
+.index-subtitle-text {
+  color: #fff;
+  font-size: 25px;
 }
 </style>
